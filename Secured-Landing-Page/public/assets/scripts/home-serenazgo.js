654559745e9b698c308e2a64af674e6a1bbb1460
Guillermo Howard <u202222275@upc.edu.pe>
2025-12-03 21:30:57 -0500

    feat: add home-serenazgo.

---
+++ Secured-Landing-Page/public/assets/scripts/home-serenazgo.js
@@ -0,0 +1,7 @@
+const btn = document.getElementById("menuBtn");
+const sidebar = document.getElementById("sidebar");
+
+// Toggle del menú lateral
+btn.addEventListener("click", () => {
+    sidebar.classList.toggle("open");
+});
